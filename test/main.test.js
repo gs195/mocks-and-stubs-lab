1574ffb5515655c8cfb03641c88a30505da3e55f
gs195 <timedilation01@gmail.com>
2019-06-03 14:06:06 +0800

    assignment-2

--- test/main.test.js
+++ test/main.test.js
@@ -1,10 +1,41 @@
+const mockMakePayment = jest.fn();
+const mockMakeRefund = jest.fn();
+const mockQueue = jest.fn();
+
+jest.doMock("../src/paymentService.js", () => {
+  return {
+    makePayment: mockMakePayment,
+    refundPayment: mockMakeRefund
+  };
+});
+
+jest.doMock("../src/queueService.js", () => {
+  return mockQueue;
+});
+
 const processPayments = require("../src/main");
 
-test.skip('does not call makePayment or refundPayment when paymentQueue is empty', () => {
+beforeEach(() => {
+  mockMakePayment.mockClear();
+  mockMakeRefund.mockClear();
+  mockQueue.mockClear();
 });
 
-test.skip('calls makePayment when next item in paymentQueue is positive', () => {
+test("does not call makePayment or refundPayment when paymentQueue is empty", () => {
+  mockQueue.mockReturnValue([]);
+  processPayments();
+  expect(mockMakePayment).not.toHaveBeenCalled();
+  expect(mockMakeRefund).not.toHaveBeenCalled();
 });
 
-test.skip('calls refundPayment when next item in paymentQueue is negative', () => {
+test("calls makePayment when next item in paymentQueue is positive", () => {
+  mockQueue.mockReturnValue([2, 5]);
+  processPayments();
+  expect(mockMakePayment).toHaveBeenCalled();
+});
+
+test("calls refundPayment when next item in paymentQueue is negative", () => {
+  mockQueue.mockReturnValue([-4]);
+  processPayments();
+  expect(mockMakeRefund).toHaveBeenCalled();
 });
